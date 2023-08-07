251e3cb05373656d06f6dba2948121c03604a570
Leon Parsons <MY_NAME@example.com>
2023-08-07 15:12:41 +1200

    Adds nav bar elements and basic css

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,9 +1,38 @@
+import {FiHeart} from "react-icons/fi";
+import {AiOutlineShoppingCart, AiOutlineUserAdd} from "react-icons/ai";
+
 import "./Nav.css";
 
 function Nav() {
   return (
-    <div>Nav</div>
-  )
+    <nav>
+      <div id="Logo">
+        <h1>
+          THE NEW PETS
+        </h1>
+      </div>
+      <div className="nav-container">
+        <input className="search-input"
+          type="text" 
+          placeholder="Search all products..."
+        />
+      </div>
+
+      <div className="profile-container">
+        <a href="#">
+        <FiHeart className="nav-icons"/>
+        </a>
+
+        <a href="#">
+          <AiOutlineShoppingCart className="nav-icons"/>
+        </a>
+
+        <a href="#">
+          <AiOutlineUserAdd className="nav-icons"/>
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
