b1a1c2e7ccd7bf9e755df0a0b79a91bab4fca2d1
Leon Parsons <MY_NAME@example.com>
2023-08-08 14:32:46 +1200

    Adds elements of recomended section

--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -2,7 +2,16 @@ import "./Recommended.css"
 
 function Recommended() {
   return (
-    <div>Recommended</div>
+    <div>
+      <h2 className="recommended-title">Recommended</h2>
+        <div className="recommended-flex">
+          <button className="btns">All Products</button>
+          <button className="btns">Cacti</button>
+          <button className="btns">Ferns</button>
+          <button className="btns">Bamboo</button>
+          <button className="btns">Succulents</button>
+        </div>
+    </div>
   )
 }
 
