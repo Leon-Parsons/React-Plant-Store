cd04c45c67ea763d229eae4d0fee8fdf9b3a5ff0
Leon Parsons <MY_NAME@example.com>
2023-08-09 17:44:57 +1200

    adds basic sidebar elements/category radio buttons

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -3,7 +3,26 @@ import "./Category.css";
 function Category() {
   return (
     <div>
-      
+      <h2 className="sidebar-title">Category </h2>
+
+      <div>
+        <label className="sidebar-label-container">
+        <input type="radio" name="test"/>
+        <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+        <input type="radio" name="test"/>
+        <span className="checkmark"></span>Cacti
+        </label>
+        <label className="sidebar-label-container">
+        <input type="radio" name="test"/>
+        <span className="checkmark"></span>Ferns
+        </label>
+        <label className="sidebar-label-container">
+        <input type="radio" name="test"/>
+        <span className="checkmark"></span>Bamboo
+        </label>
+      </div>
     </div>
   )
 }
