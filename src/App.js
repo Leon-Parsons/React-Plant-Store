e70a6c8e992a0c001cd3cb9937991f0fee0011b4
Leon Parsons <MY_NAME@example.com>
2023-08-16 15:07:54 +1200

    Adds state and filter of products for search bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import Sidebar from "./Sidebar/Sidebar";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
+import { useState } from "react";
+
+import products from "./product_data/data";
 
 function App() {
+  //search bar state
+  const [query, setQuery] = useState("");
+  //search bar state set to user input
+  const handleInputChange = event => {
+    setQuery(event.target.value)
+  }
+  //product filter
+  const filteredItems = products.filter(product => 
+    product.title.toLowerCase().indexOf(query.toLowerCase() !== -1)
+  );
+
   return (
     <>
-    <Sidebar/>
+      <Sidebar/>
       <Nav/>
       <Recommended/>
       <Products/>
